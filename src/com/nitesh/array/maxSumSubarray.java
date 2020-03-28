8e1eee563243a0866c12bbc7f85bb1158626d217
Nitesh Sinha <mit.nitesh@gmail.com>
2020-03-28 17:26:53 +0530

    maximum sum subarray problem

---
+++ src/com/nitesh/array/maxSumSubarray.java
@@ -0,0 +1,26 @@
+//        Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+//
+//        Example:
+//
+//        Input: [-2,1,-3,4,-1,2,1,-5,4],
+//        Output: 6
+//        Explanation: [4,-1,2,1] has the largest sum = 6.
+
+
+package com.nitesh.array;
+
+public class maxSumSubarray {
+    public int maxSumSubArrayFn(int[] nums) {
+        int sumTillNow=0, maxSum=Integer.MIN_VALUE;
+
+        for(int n:nums) {
+            sumTillNow+=n;
+            // if current number > (sum of relevant numbers seen before+current num)
+            // then ignore the numbers seen before in the maximum subarray
+            sumTillNow = Math.max(sumTillNow, n);
+            maxSum = Math.max(maxSum, sumTillNow);
+        }
+        return maxSum;
+    }
+
+}
