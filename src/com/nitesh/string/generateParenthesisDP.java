221dbb4f0ecc69414ffd9d3b89aedcaf4faad2be
Nitesh Sinha <mit.nitesh@gmail.com>
2020-04-10 11:50:03 +0530

    Explain the steps of the DP parens problem

--- src/com/nitesh/string/generateParenthesisDP.java
+++ src/com/nitesh/string/generateParenthesisDP.java
@@ -13,11 +13,17 @@ public class generateParenthesisDP {
             int parenLen;
             Set<String> parenSet = new HashSet<>();
             String newParen;
+            // Algorithm:
+            // 1. Get balanced parenthesis list for n-1 given n
+            // 2. For every such balanced parenthesis, do the following:
+            // 2a.      Take first character of first balanced parenthesis expression, add (), then add remaining chars from the first expression.
+            // 2b.      Take first 2 chars of first balanced parenthesis expression, add (), then add remaining chars from the first expression.
+            // 2c.      Repeat this process until end of first balanced parenthesis expression.
+            // 2d.      Repeat this process for all the remaining balanced parens expressions obtained from Step 1.
+            // 3.       Store these in a Set since there will be duplicates as you perform steps 2a thru d. So, a Set will eliminate duplicates.
             List<String> parensList = generateParenthesisDPFn(n-1);
             for(String paren : parensList) {
                 parenLen = paren.length();
-                //char[] parenChrs = new char[parenLen];
-                //parenChrs = paren.toCharArray();
                 for(int i=0; i< parenLen; i++) {
                     newParen = paren.substring(0, i+1) + "()" + paren.substring(i+1, parenLen);
                     parenSet.add(newParen);
