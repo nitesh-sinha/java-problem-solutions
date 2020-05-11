2f263a207ba5e60e433e5d508f03b57b08f31b55
Nitesh Sinha <mit.nitesh@gmail.com>
2020-05-11 21:40:04 +0530

    Dynamic prog - intermediate problems

---
+++ src/com/nitesh/array/combinationSum4.java
@@ -0,0 +1,48 @@
+//        Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
+//
+//        Example:
+//
+//        nums = [1, 2, 3]
+//        target = 4
+//
+//        The possible combination ways are:
+//        (1, 1, 1, 1)
+//        (1, 1, 2)
+//        (1, 2, 1)
+//        (1, 3)
+//        (2, 1, 1)
+//        (2, 2)
+//        (3, 1)
+//
+//        Note that different sequences are counted as different combinations.
+//
+//        Therefore the output is 7.
+
+// Time complexity: O(nk) where n=length of nums; k=target
+
+package com.nitesh.array;
+
+import java.util.Arrays;
+
+public class combinationSum4 {
+    public int combinationSum4Fn(int[] nums, int target) {
+        // Dynamic programming solution
+        if(target <= 0)
+            return 0;
+        int sumRemainder;
+
+        Arrays.sort(nums);
+
+        int[] numCombo = new int[target+1]; // numCombo[i] stores the num of possible sequences where each of them add up to i
+        numCombo[0] = 1;
+
+        for(int i = 1; i<= target; i++) {
+            numCombo[i] = 0;
+            for(int j = 0; j<nums.length && nums[j] <= i; j++) {
+                sumRemainder = i-nums[j];
+                numCombo[i] += numCombo[sumRemainder];
+            }
+        }
+        return numCombo[target];
+    }
+}
---
+++ src/com/nitesh/array/numSquares.java
@@ -0,0 +1,37 @@
+//        Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+//
+//        Example 1:
+//
+//        Input: n = 12
+//        Output: 3
+//        Explanation: 12 = 4 + 4 + 4.
+//        Example 2:
+//
+//        Input: n = 13
+//        Output: 2
+//        Explanation: 13 = 4 + 9.
+
+// Time complexity: O(n * sqrt(n)) where n= input number; sqrt(n) =no. of perfect squares less than or equal to n
+
+package com.nitesh.array;
+
+public class numSquares {
+    // For more info: http://www.geeksforgeeks.org/minimum-number-of-squares-whose-sum-equals-to-given-number-n/
+    // Dynamic programming solution is a lot faster for n>50
+    public int numSquaresFn(int n) {
+        // Dynamic programming solution
+        if(n<=0)
+            return 0;
+
+        int[] numPerfectSq = new int[n+1]; // numPerfectSq[i] stores min. no. of perfect squares that add up to i
+        numPerfectSq[0] = 0;
+        numPerfectSq[1] = 1;
+
+        for(int i=2 ; i<= n; i++) {
+            numPerfectSq[i] = Integer.MAX_VALUE;
+            for(int j=1; j*j <= i; j++)
+                numPerfectSq[i] = Math.min(numPerfectSq[i], 1 + numPerfectSq[i-j*j]); // Adding 1 here to consider the term (j*j) that we subtract from i here
+        }
+        return numPerfectSq[n];
+    }
+}
