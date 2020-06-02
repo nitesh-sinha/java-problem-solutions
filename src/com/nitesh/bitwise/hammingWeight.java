775035fccdd1f1be9762e4902fed8def630ca35a
Nitesh Sinha <mit.nitesh@gmail.com>
2020-06-02 16:48:25 +0530

    count number of bits in input number

---
+++ src/com/nitesh/bitwise/hammingWeight.java
@@ -0,0 +1,23 @@
+// Hamming weight of a number is the number of set bits in the binary representation of that number. Find the hamming weight of an input number n.
+//
+//   Example 1:
+//              Input: n = 25
+//              Output: 3
+//              Explanation: 24 = Since there are 3 bits set in binary representation of 25 which is 11001
+
+package com.nitesh.bitwise;
+
+public class hammingWeight {
+    public int hammingWeightFn(int n) {
+        int count;
+        for(count = 0; n > 0; count++)
+            n = n & (n-1); // this resets the rightmost set bit in n to zero
+        return count;
+    }
+}
+
+
+
+//  25 & 24 = 11001 & 11000 = 11000 (= 24), count = 1
+//  24 & 23 = 11000 & 10111 = 10000 (= 16), count = 2
+//  16 & 15 = 10000 & 01111 = 00000 (= 0),  count = 3
--- src/com/nitesh/bitwise/testBitwise.java
+++ src/com/nitesh/bitwise/testBitwise.java
@@ -2,7 +2,9 @@ package com.nitesh.bitwise;
 
 public class testBitwise {
     public void testfn() {
-        addTwoNumbers x = new addTwoNumbers();
-        System.out.println(x.addTwoNumbersFn(1000,2001));
+//        addTwoNumbers x = new addTwoNumbers();
+//        System.out.println(x.addTwoNumbersFn(1000,2001));
+        hammingWeight x = new hammingWeight();
+        System.out.println(x.hammingWeightFn(32567));
     }
 }
